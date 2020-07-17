1f17c50d8f51ec84a203dea8059de2abdf5086ba
dishonestea <vishay8@gmail.com>
2020-07-17 16:46:09 -0500

    Updates

---
+++ precache-manifest.59e04a8e18ab0b92dbf7815f7c693f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7bc98614516a2094fc0f9caf3d6ba4",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "1e3dc1ad6f9a21021c78",
+    "url": "/quiz/static/css/main.37012a95.chunk.css"
+  },
+  {
+    "revision": "7b082f7ac77c577c387b",
+    "url": "/quiz/static/js/2.63cab0de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.63cab0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3dc1ad6f9a21021c78",
+    "url": "/quiz/static/js/main.5b33218b.chunk.js"
+  },
+  {
+    "revision": "5f7fa855701e7f8e641f",
+    "url": "/quiz/static/js/runtime-main.e79fbecc.js"
+  }
+]);
